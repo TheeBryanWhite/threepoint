6cc5703f78fe600f23aadb8b6a10f3489ac40624
Bryan White <bwhite@eastcoastcatalyst.com>
2020-05-23 18:57:47 -0400

    Fixed more conflicts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-<<<<<<< HEAD
 import { graphql } from "gatsby"
-=======
-import { graphql } from "gatsby";
->>>>>>> 0a0aa28532f2caa6ea02f5b4dde684611640f361
 
 import HomeLayout from '../components/layout/home-layout'
 import SEO from '../components/seo'
@@ -14,13 +10,8 @@ import '../components/sass/home.scss'
 
 const IndexPage = ({data}) => {
 
-<<<<<<< HEAD
   const doc = data.prismic.allPages.edges.slice(0,1).pop()
   if (!doc) return null
-=======
-  const doc = data.prismic.allPages.edges.slice(0,1).pop();
-  if (!doc) return null;
->>>>>>> 0a0aa28532f2caa6ea02f5b4dde684611640f361
 
   return (
     <HomeLayout>
@@ -31,11 +22,7 @@ const IndexPage = ({data}) => {
 }
 
 export const query = graphql`
-<<<<<<< HEAD
 query PageQuery($uid: String) {
-=======
-query HomeQuery($uid: String) {
->>>>>>> 0a0aa28532f2caa6ea02f5b4dde684611640f361
   prismic {
     allPages(uid: $uid) {
       edges {
